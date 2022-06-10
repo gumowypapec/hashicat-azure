ce7271a1edcd424270d43fd2c34e1e84522c7a7b
Piotr <piotr.kropisz@kyndryl.com>
2022-06-10 11:50:46 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/Piotro/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.example.name
+  # insert required variables here
+}
